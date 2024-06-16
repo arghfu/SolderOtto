f6b3000f56197211e07b7b5fffa3c373495e1b9b
Sebastian Belz <belz.sebastian@googlemail.com>
2024-06-16 09:38:36 +0200

    working dma

--- app/src/main.c
+++ app/src/main.c
@@ -10,6 +10,8 @@
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/dma.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/drivers/misc/ft8xx/ft8xx.h>
+
 
 uint64_t last_time = 0;
 
@@ -35,32 +37,24 @@ struct gpio_callback zcd_cb_data;
 void zcd_callback(const struct device *dev,
 	struct gpio_callback *cb, uint32_t pins)
 {
-	uint64_t now = k_uptime_get();
+	uint64_t now = k_cycle_get_64();
 
 	//TODO do stuff
 
+	uint64_t diff = now - last_time;
 	last_time = now;
+
+	diff = k_cyc_to_us_floor64(diff);
+	// printk("half wave time: %"PRId64" us\n", diff);
 }
 
-#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
-!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
-#error "No suitable devicetree overlay specified"
-#endif
-
-#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
-ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
-
-/* Data of ADC io-channels specified in devicetree. */
-static const struct adc_dt_spec adc_channels[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
-				 DT_SPEC_AND_COMMA)
-};
+static const struct adc_dt_spec adc_tip_temp =
+	ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), ch0_tip);
 
 int main(void)
 {
 	bool led_state = false;
 	int err;
-	uint32_t count = 0;
 	uint16_t buf;
 	struct adc_sequence sequence = {
 		.buffer = &buf,
@@ -68,21 +62,22 @@ int main(void)
 		.buffer_size = sizeof(buf),
 	};
 
-	/* Configure channels individually prior to sampling. */
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!adc_is_ready_dt(&adc_channels[i])) {
-			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
-			return 0;
-		}
-
-		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
-			return 0;
-		}
+	/* Configure channel individually prior to sampling. */
+	if (!adc_is_ready_dt(&adc_tip_temp)) {
+		printk("ADC controller device %s not ready\n", adc_tip_temp.dev->name);
+		return 0;
 	}
+
+	err = adc_channel_setup_dt(&adc_tip_temp);
+	if (err < 0) {
+		printk("Could not setup channel Ch0-tip (%d)\n", err);
+		return 0;
+	}
+
 	LOG_INF("Device Ready");
 	LOG_INF("Zephyr Example Application %s", APP_VERSION_STRING);
+
+	(void)adc_sequence_init_dt(&adc_tip_temp, &sequence);
 
 	// configure button pin as input
 	gpio_pin_configure_dt(&zeroCross, GPIO_INPUT);
@@ -127,51 +122,52 @@ int main(void)
 		// 	k_sleep(K_MSEC(5000));
 		// }
 
-		// T210
-		if(led_state == true)
-		{
-			// gpio_pin_set_dt(&com_low, GPIO_PIN_SET);
-			// gpio_pin_set_dt(&load_high, GPIO_PIN_SET);
-			k_sleep(K_MSEC(40));
-		}
-		else
-		{
-			gpio_pin_set_dt(&load_high, GPIO_PIN_RESET);
-			k_sleep(K_MSEC(5000));
-		}
+		// // T210
+		// if(led_state == true)
+		// {
+		// 	// gpio_pin_set_dt(&com_low, GPIO_PIN_SET);
+		// 	// gpio_pin_set_dt(&load_high, GPIO_PIN_SET);
+		// 	k_sleep(K_MSEC(40));
+		// }
+		// else
+		// {
+		// 	gpio_pin_set_dt(&load_high, GPIO_PIN_RESET);
+		// 	k_sleep(K_MSEC(5000));
+		// }
 
 		led_state = !led_state;
 
-		// printk("ADC reading[%u]:\n", count++);
-		// for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		// 	int32_t val_mv;
-		//
-		// 	printk("- %s, channel %d: ",
-		// 		   adc_channels[i].dev->name,
-		// 		   adc_channels[i].channel_id);
-		//
-		// 	(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
-		//
-		// 	err = adc_read_dt(&adc_channels[i], &sequence);
-		// 	if (err < 0) {
-		// 		printk("Could not read (%d)\n", err);
-		// 		continue;
-		// 	}
-		//
-		// 	val_mv = (int32_t)buf;
-		//
-		// 	printk("%"PRId32, val_mv);
-		// 	err = adc_raw_to_millivolts_dt(&adc_channels[i],
-		// 					   &val_mv);
-		// 	/* conversion to mV may not be supported, skip if not */
-		// 	if (err < 0) {
-		// 		printk(" (value in mV not available)\n");
-		// 	} else {
-		// 		printk(" = %"PRId32" mV\n", val_mv);
-		// 	}
-		// }
-		//
-		// k_sleep(K_MSEC(400));
+		int32_t val_mv;
+			printk("- %s, channel %d: ",
+				   adc_tip_temp.dev->name,
+				   adc_tip_temp.channel_id);
+
+
+
+		uint64_t begin = k_cycle_get_64();
+		err = adc_read_dt(&adc_tip_temp, &sequence);
+		uint64_t end = k_cycle_get_64();
+		uint64_t diff = k_cyc_to_us_floor64(end - begin);
+
+		if (err < 0) {
+			printk("Could not read (%d)\n", err);
+			continue;
+		}
+
+		printk("time to covert: %"PRId64" \n", diff);
+		val_mv = (int32_t)buf;
+
+		printk("%"PRId32, val_mv);
+		err = adc_raw_to_millivolts_dt(&adc_tip_temp,
+						   &val_mv);
+		/* conversion to mV may not be supported, skip if not */
+		if (err < 0) {
+			printk(" (value in mV not available)\n");
+		} else {
+			printk(" = %"PRId32" mV\n", val_mv);
+		}
+
+		k_sleep(K_MSEC(1));
 	}
 
 	return 0;
