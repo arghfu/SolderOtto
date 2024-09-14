f781ac510cc38f64a1c3e01ddf2a0c622c7aa432
Sebastian Belz <belz.sebastian@googlemail.com>
2024-09-14 12:35:01 +0200

    Revert "try to get the 817 running"
    This reverts commit 53aea3062e6a234821b44acb40f46de12e78a91a.

--- app/src/main.c
+++ app/src/main.c
@@ -10,33 +10,7 @@
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/spi.h>
 
-#include <app/drivers/ft81x.h>
-#include <app/drivers/ft81x_copro.h>
-#include <app/drivers/ft81x_dl.h>
-
-#include <zephyr/drivers/misc/ft8xx/ft8xx.h>
-#include <zephyr/drivers/misc/ft8xx/ft8xx_copro.h>
-#include <zephyr/drivers/misc/ft8xx/ft8xx_dl.h>
-
-/**
- * @file Display a counter using FT800.
- */
-
-/* sleep time in msec */
-#define SLEEPTIME  100
-
-/* touch tags */
-#define TAG_PLUS 1
-#define TAG_MINUS 2
-
-static volatile bool process_touch;
-
 static const struct gpio_dt_spec dbg0 = GPIO_DT_SPEC_GET(DT_ALIAS(dbg_0), gpios);
-
-static void touch_irq(void)
-{
-	process_touch = true;
-}
 
 int main(void)
 {
@@ -44,72 +18,7 @@ int main(void)
 
 	gpio_pin_configure_dt(&dbg0, (GPIO_OUTPUT |  GPIO_OPEN_DRAIN) );
 
-
-	// int cnt;
-	// int val;
-	// struct ft8xx_touch_transform tt;
-	//
-	// /* To get touch events calibrate the display */
-	// ft8xx_calibrate(&tt);
-	//
-	// /* Get interrupts on touch event */
-	// ft8xx_register_int(touch_irq);
-	//
-	// /* Starting counting */
-	// val = 0;
-	// cnt = 0;
-	//
-	// while (1) {
-	// 	/* Start Display List */
-	// 	ft8xx_copro_cmd_dlstart();
-	// 	ft8xx_copro_cmd(FT8XX_CLEAR_COLOR_RGB(0x00, 0x00, 0x00));
-	// 	ft8xx_copro_cmd(FT8XX_CLEAR(1, 1, 1));
-	//
-	// 	/* Set color */
-	// 	ft8xx_copro_cmd(FT8XX_COLOR_RGB(0xf0, 0xf0, 0xf0));
-	//
-	// 	/* Display the counter */
-	// 	ft8xx_copro_cmd_number(20, 20, 29, FT8XX_OPT_SIGNED, cnt);
-	// 	cnt++;
-	//
-	// 	/* Display the hello message */
-	// 	ft8xx_copro_cmd_text(20, 70, 30, 0, "Hello,");
-	// 	/* Set Zephyr color */
-	// 	ft8xx_copro_cmd(FT8XX_COLOR_RGB(0x78, 0x29, 0xd2));
-	// 	ft8xx_copro_cmd_text(20, 105, 30, 0, "Zephyr!");
-	//
-	// 	/* Display value set with buttons */
-	// 	ft8xx_copro_cmd(FT8XX_COLOR_RGB(0xff, 0xff, 0xff));
-	// 	ft8xx_copro_cmd_number(80, 170, 29,
-	// 				FT8XX_OPT_SIGNED | FT8XX_OPT_RIGHTX,
-	// 				val);
-	// 	ft8xx_copro_cmd(FT8XX_TAG(TAG_PLUS));
-	// 	ft8xx_copro_cmd_text(90, 160, 31, 0, "+");
-	// 	ft8xx_copro_cmd(FT8XX_TAG(TAG_MINUS));
-	// 	ft8xx_copro_cmd_text(20, 160, 31, 0, "-");
-	//
-	// 	/* Finish Display List */
-	// 	ft8xx_copro_cmd(FT8XX_DISPLAY());
-	// 	/* Display created frame */
-	// 	ft8xx_copro_cmd_swap();
-	//
-	// 	if (process_touch) {
-	// 		int tag = ft8xx_get_touch_tag();
-	//
-	// 		if (tag == TAG_PLUS) {
-	// 			val++;
-	// 		} else if (tag == TAG_MINUS) {
-	// 			val--;
-	// 		}
-	//
-	// 		process_touch = false;
-	// 	}
-	//
-	// 	/* Wait a while */
-	// 	k_msleep(SLEEPTIME);
-	// }
-
-	printk("Pin config failed\n");
+	printk("Pin config failed:");
 	while (1)
 	{
 		gpio_pin_toggle_dt(&dbg0);
